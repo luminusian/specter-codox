2483a97388aafa92d6d5dc88b64b031aedb1a1f2
Your Name <you@example.com>
2017-06-07 08:29:17 +0000

    Call setval

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [com.rpl/specter "1.0.1"]]
   :main ^:skip-aot specter-codox.core
   :target-path "target/%s"
+  :codox {:language :clojurescript}
+  :plugins [[lein-codox "0.10.3"]]
   :profiles {:uberjar {:aot :all}})
--- src/specter_codox/core.cljs
+++ src/specter_codox/core.cljs
@@ -1,7 +1,10 @@
 (ns specter-codox.core
+  (:require [com.rpl.specter :as s :include-macros true])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(s/setval s/END [:a] [])
