6fbd2570eea724ed85833253d4c342b16fdf5c80
Your Name <you@example.com>
2017-06-11 04:46:59 +0000

    Add codox to :dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.562"]
-                 [com.rpl/specter "1.0.1"]]
+                 [com.rpl/specter "1.0.1"]
+                 [codox "0.10.3"]]
   :main ^:skip-aot specter-codox.core
   :target-path "target/%s"
   :codox {:language :clojurescript}
